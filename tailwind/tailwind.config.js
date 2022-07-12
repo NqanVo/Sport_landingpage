ddb94bd3076a271a89479a9007392219b7cbfaf9
NqanVo <2ngres@gmail.com>
2022-07-12 22:35:38 +0700

    add responsive + animation

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../*.html"],
+  darkMode: false,
+  mode: 'jit',
+  purge: ["../*.html"],
+  content: ["../*.html","../**/**/*.js"],
   theme: {
     extend: {},
   },
